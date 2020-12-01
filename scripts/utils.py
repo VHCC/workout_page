72b13f4f2228adb87a45e8b251c478ead094f28e
dengzhenpeng <ben_29@live.com>
2020-12-01 16:11:07 +0800

    add update activities.js script

--- scripts/utils.py
+++ scripts/utils.py
@@ -13,3 +13,13 @@ def make_activities_file(sql_file, gpx_dir, json_file):
         f.write(";\n")
         f.write("\n")
         f.write("export {activities};\n")
+
+def make_activities_file_only(sql_file, json_file):
+    generator = Generator(sql_file)
+    activities_list = generator.loadForMapping()
+    with open(json_file, "w") as f:
+        f.write("const activities = ")
+        json.dump(activities_list, f, indent=2)
+        f.write(";\n")
+        f.write("\n")
+        f.write("export {activities};\n")
