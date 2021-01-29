c8db9caef11439c39c02d48232bc42e48a4eb5f7
ben_29 <ben_29@live.com>
2021-01-29 17:25:46 +0800

    add WorkoutStat from avg pace

--- src/components/YearStat/index.jsx
+++ src/components/YearStat/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import useHover from 'src/hooks/useHover';
 import Stat from 'src/components/Stat';
+import WorkoutStat from 'src/components/WorkoutStat';
 import { formatPace } from 'src/utils/utils';
 import useActivities from 'src/hooks/useActivities';
 import styles from './style.module.scss';
@@ -21,25 +22,30 @@ const YearStat = ({ year, onClick }) => {
   }
   let sumDistance = 0;
   let streak = 0;
-  let RunPace = 0;
-  let RunCount = 0;
-  let RidePace = 0;
-  let RideCount = 0;
   let heartRate = 0;
   let heartRateNullCount = 0;
+  let countsMap = new Map();
+  let avgSpdMap = new Map();
+  let distanceMap = new Map();
   runs.forEach((run) => {
     sumDistance += run.distance || 0;
     if (run.average_speed) {
-      switch(run.type){
-        case 'Run':
-          RunPace += run.average_speed;
-          RunCount ++;
-          break;
-        case 'Ride':
-          RidePace += run.average_speed;
-          RideCount ++;
-          break;
+      let type = run.type;
+      let count = 1;
+      let avgSpd = run.average_speed;
+      let distance = run.distance;
+      if(countsMap.has(type)){
+        count += countsMap.get(type);
       }
+      if(avgSpdMap.has(type)){
+        avgSpd += avgSpdMap.get(type);
+      }
+      if(distanceMap.has(type)){
+        distance += distanceMap.get(type);
+      }
+      countsMap.set(type, count)
+      avgSpdMap.set(type, avgSpd)
+      distanceMap.set(type, distance)
     }
     if (run.average_heartrate) {
       heartRate += run.average_heartrate;
@@ -51,14 +57,6 @@ const YearStat = ({ year, onClick }) => {
     }
   });
   sumDistance = (sumDistance / 1000.0).toFixed(1);
-  if( RunCount > 0){
-    RunPace = RunPace / RunCount;
-  }
-  if( RideCount > 0){
-    RidePace = RidePace / RideCount;
-  }
-  const avgRunningPace = formatPace(RunPace);
-  const avgRidingPace = formatPace(RidePace);
   const hasHeartRate = !(heartRate === 0);
   const avgHeartRate = (heartRate / (runs.length - heartRateNullCount)).toFixed(
     0
@@ -71,10 +69,14 @@ const YearStat = ({ year, onClick }) => {
     >
       <section>
         <Stat value={year} description=" Journey" />
-        <Stat value={runs.length} description=" Workouts" />
+        { countsMap.has('Run') && (<WorkoutStat value={`${countsMap.get('Run')}`} description=" Runs" pace={formatPace(avgSpdMap.get('Run') / countsMap.get('Run'))} /* distance={(distanceMap.get('Run')/1000).toFixed(1)} *//>)}
+        { countsMap.has('Ride') && (<WorkoutStat value={`${countsMap.get('Ride')}`} description=" Rides" pace={formatPace(avgSpdMap.get('Ride') / countsMap.get('Ride'))} /* distance={(distanceMap.get('Ride')/1000).toFixed(1)} *//>)}
+        { countsMap.has('Hike') && (<WorkoutStat value={`${countsMap.get('Hike')}`} description=" Hikes" pace={formatPace(avgSpdMap.get('Hike') / countsMap.get('Hike'))} /* distance={(distanceMap.get('Hike')/1000).toFixed(1)} *//>)}
+        { countsMap.has('Swim') && (<WorkoutStat value={`${countsMap.get('Swim')}`} description=" Swims" pace={formatPace(avgSpdMap.get('Swim') / countsMap.get('Swim'))} /* distance={(distanceMap.get('Swim')/1000).toFixed(1)} */ />)}
+        { countsMap.has('Rowing') && (<WorkoutStat value={`${countsMap.get('Rowing')}`} description=" Rowings" pace={formatPace(avgSpdMap.get('Rowing') / countsMap.get('Rowing')) } /* distance={(distanceMap.get('Rowing')/1000).toFixed(1)} *//>)}
         <Stat value={sumDistance} description=" KM" />
-        { avgRunningPace != '0' && (<Stat value={avgRunningPace} description=" Avg Running Pace" />)}
-        { avgRidingPace != '0' && (<Stat value={avgRidingPace} description=" Avg Riding Pace" />)}
+        {/* { avgRunningPace != '0' && (<Stat value={avgRunningPace} description=" Avg Running Pace" />)}
+        { avgRidingPace != '0' && (<Stat value={avgRidingPace} description=" Avg Riding Pace" />)} */}
         <Stat
           value={`${streak} day`}
           description=" Streak"
