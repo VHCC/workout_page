d4e61886ce99a10736f1cd5977deb76e123e339d
ben_29 <ben_29@live.com>
2021-09-25 16:01:49 +0800

    style: workouts stat

--- src/components/WorkoutStat/index.jsx
+++ src/components/WorkoutStat/index.jsx
@@ -7,11 +7,11 @@ const WorkoutStat = ({ value, description, pace, className, distance, onClick })
     <span className={`f1 fw9 i`}>{intComma(value)}</span>
     <span className="f3 fw6 i">{description}</span>
     { pace && (<span className="f1 fw9 i">{ " " +pace}</span>)}
-    { pace && (<span className="f3 fw6 i"> Avg Pace</span>)}
+    { pace && (<span className="f3 fw6 i"> Pace</span>)}
 
     { distance && (<span className="f1 fw9 i">{ " " + distance}</span>)}
     { distance && (<span className="f3 fw6 i"> KM</span>)}
-    
+
   </div>
 );
 
