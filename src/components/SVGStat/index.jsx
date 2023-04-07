e9528867cfa6601c5174b30df28950583c55aa96
Frank Chu <r99521320@gmail.com>
2023-04-07 10:13:38 +0800

    Update index.jsx

--- src/components/SVGStat/index.jsx
+++ src/components/SVGStat/index.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import GitHubSvg from 'assets/github.svg';
+//import GitHubSvg from 'assets/github.svg';
 import GridSvg from 'assets/grid.svg';
 import styles from './style.module.scss';
 
 const SVGStat = () => (
   <div>
     <GridSvg className={styles.runSVG} />
-    <GitHubSvg className={styles.runSVG} />
+    //<GitHubSvg className={styles.runSVG} />
   </div>
 );
 
